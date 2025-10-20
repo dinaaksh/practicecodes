16bc5ca455171418ee9475b7289fdb6feffde093
dinaaksh <76864623+dinaaksh@users.noreply.github.com>
2025-10-20 09:46:13 +0530

    Create assign_cookies.cpp

---
+++ greedy_algorithms/assign_cookies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int count=0;
+        int j=0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        for(int i=0;i<s.size()&&j<g.size();i++){
+            if(s[i]>=g[j]){
+                j++;
+                count++;
+            }
+        }
+        return count;
+    }
+};
