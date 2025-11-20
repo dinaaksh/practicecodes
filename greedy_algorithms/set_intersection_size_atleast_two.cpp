759d8c4db903646ddd7e64e50f85ab1a6d620212
dinaaksh <76864623+dinaaksh@users.noreply.github.com>
2025-11-20 17:54:30 +0530

    Create set_intersection_size_atleast_two.cpp

---
+++ greedy_algorithms/set_intersection_size_atleast_two.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    static bool comparator(const vector<int> &a,const vector<int> &b){
+        if(a[1]==b[1]) return a[0]>b[0];
+        return a[1]<b[1];
+    }
+    int intersectionSizeTwo(vector<vector<int>>& intervals){
+        sort(intervals.begin(),intervals.end(),comparator);
+        vector<int> ans;
+        for(auto& temp:intervals){
+            int start=temp[0];
+            int end=temp[1];
+            if(ans.size()>=2 && ans.back()>=start && ans[ans.size()-2]>=start)
+                continue;
+            if(ans.empty() || ans.back()<start){
+                ans.push_back(end-1);
+                ans.push_back(end);
+            } 
+            else{
+                ans.push_back(end);
+            }
+        }
+        return ans.size();
+    }
+};
