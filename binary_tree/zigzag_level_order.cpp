2134964401e8c722ebfeeab7eecbffe6a1692811
dinaaksh <76864623+dinaaksh@users.noreply.github.com>
2025-11-22 22:42:01 +0530

    Create zigzag_level_order.cpp

---
+++ binary_tree/zigzag_level_order.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if(root==nullptr) return {};
+        queue<TreeNode*> q;
+        vector<vector<int>> ans;
+        q.push(root);
+        bool flag=true;
+        while(!q.empty()){
+            int n=q.size();
+            vector<int> res(n);
+            for(int i=0;i<n;i++){
+                TreeNode* temp=q.front();
+                q.pop();
+                int level=flag?i:(n-1-i);
+                res[level]=temp->val;
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+            flag=!flag;
+            ans.push_back(res);
+        }
+        return ans;
+    }
+};
