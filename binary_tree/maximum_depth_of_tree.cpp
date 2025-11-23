4b57a3d19ef002686dd9a2f3493c7f2005af6f22
dinaaksh <76864623+dinaaksh@users.noreply.github.com>
2025-11-23 11:54:32 +0530

    Update maximum_depth_of_tree.cpp

--- binary_tree/maximum_depth_of_tree.cpp
+++ binary_tree/maximum_depth_of_tree.cpp
@@ -24,3 +24,14 @@ public:
         return maxcount;
     }
 };
+
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root) {
+//         if(root==nullptr) return 0;
+//         int left=maxDepth(root->left);
+//         int right=maxDepth(root->right);
+//         return 1+max(left,right);
+//     }
+// };
+// OR JUST DO THIS^^^^^^^^^^^^
